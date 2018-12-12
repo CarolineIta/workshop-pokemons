b625cee34eb3ef79efee3e081d0fc92273e17347
caroline-ita <caroline.mg.ita@gmail.com>
2018-12-12 00:17:19 -0200

    Ajustes no env.

--- features/step_definitions/inserepokemon.rb
+++ features/step_definitions/inserepokemon.rb
@@ -5,17 +5,17 @@ Dado("que estou na página da  pokedex") do
    E("clico para adicionar um novo pokemon") do
      find('.btn-success').click
    end
+   
    Quando("preencho todas as informações") do
      find_field('id').set(0001)
      find_field('name').set('Zé')
      find_field('type1').find('option', text: 'Voador').select_option
      find_field('type2').find('option', text: 'Veneno').select_option
      find_field('attack').set(10)
-      find_field('defense').set(5)
-     sleep(2)  
-     find_field('hp').set(100)
-     sleep(2)  
+     find_field('defense').set(5)
+     find_field('hp').set(100)   
    end
+   
      E("clico em Salvar") do
      click_button('Salvar')
     end
--- features/support/env.rb
+++ features/support/env.rb
@@ -9,10 +9,10 @@ require 'capybara/dsl'
 
 Capybara.register_driver :selenium_chrome do |app|
     arguments = ["no-sandbox"]
+    #arguments = ["headless","no-sandbox"]
     options = Selenium::WebDriver::Chrome::Options.new(args: arguments)
     # noinspection RubyArgCount
     Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-
 end
 
 Capybara.run_server = false
